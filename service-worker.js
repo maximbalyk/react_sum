2597fe13466fa3243cb269ae6d8c5cff555fd398
maximbalyk <maximbalyk@gmail.com>
2021-08-31 14:16:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.2f44c6c88160e54290606c97e36e8daf.js"
 );
 
 self.addEventListener('message', (event) => {
